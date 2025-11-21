f626b691a04e8e06d08cb3e8918698fbc1b87f3a
veronica860108 <veroruiba@gmail.com>
2025-11-21 20:21:34 +0000

    piedra,papel,tijeras,lagarto.spok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-import "bootstrap";
-import "./style.css";
+const choices = ["rock", "paper", "scissors", "lizard", "spock"]
+const rules = {
+  rock: ['scissors', "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["spock", "paper"],
+  spock: ["scissors", "rock"]
+}
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//computer choise
+function computerChoise() {
+  let posicionChoises = Math.floor((Math.random() * choices.length()));
+  let eleccionComputer = choices[posicionChoices];
+  return eleccionComputer;
+}
+//usuary choise
+function UsuaryChoise() {
+  console.log();
+  
+  
+}
+let computer = computerChoise();
+let player = UsuaryChoise();
+if (computer=player) {
+  console.log("que lastima es un triste empate");
+} else if (player.includes()computer) {
+  console.log("has ganado");
+}
+else{console.log("has perdido");
+}
